48d94e73da65e3b06c65e0eeab0532b698c4cd0a
DelpinSusaiRaj <susairajs@outlook.com>
2017-04-18 11:29:56 +0530

    all files added

---
+++ XamariniOSMap/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using UIKit;
+
+using Google.Maps;
+using CoreGraphics;
+
+namespace XamariniOSMap
+{
+	public partial class ViewController : UIViewController
+	{
+		MapView mapView;
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+
+			var camera = CameraPosition.FromCamera(latitude: 37.79,
+											longitude: -122.40,
+											zoom: 6);
+			mapView = MapView.FromCamera(CGRect.Empty, camera);
+			mapView.MyLocationEnabled = true;
+			View = mapView;
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
